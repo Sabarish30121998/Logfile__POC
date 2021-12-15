1f3e1bb2a8f6bb4575eae762c8b97d30826c2f70
Sabarish <sabarish30121998@gmail.com>
2021-12-15 15:02:14 +0530

    Copy file using Multipart File, Create Directory, Get Project path and create directory and upload a file using Multipart (Only editable files like txt file only)

--- src/main/java/com/example/POC_file/Controller/createcontroller.java
+++ src/main/java/com/example/POC_file/Controller/createcontroller.java
@@ -2,14 +2,15 @@ package com.example.POC_file.Controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/general")
@@ -87,5 +88,113 @@ public class createcontroller {
             throw new RuntimeException("noooooooooooooooooooo");
         }
     }
+
+
+    @GetMapping("/copyfileusingmultipart")
+    public String copyfileusingmultipart(@RequestParam MultipartFile file){
+        try {
+            byte[] array = new byte[file.getBytes().length];
+            String name = file.getOriginalFilename();
+          //  File new123 = new File("E:\\fresh files\\king.txt");
+          //  byte[] array = new byte[(int) new123.length()];
+         //   FileInputStream sourcefile = new FileInputStream("E:\\fresh files\\king.txt");
+          //  sourcefile.read(array);
+         //   System.out.println("Byte Length is " +new123.length());
+            File file1 = new File("E:\\fresh files\\"+name);
+            if(file1.createNewFile())
+            {
+                System.out.println("File is Created");
+            }
+            else {
+                return "File cannot be able to create";
+            }
+            if(file1.exists()){
+                if(file1.canWrite()){
+                    FileOutputStream destinationfile = new FileOutputStream("E:\\fresh files\\queen.txt");
+                    destinationfile.write(array);
+                    destinationfile.close();
+                }else {
+                    return "File exist but cannot able write";
+                }
+            }
+            else {
+                return "File is Not Exist";
+            }
+         //   sourcefile.close();
+            return "Success";
+        }
+        catch (Exception e){
+            throw new RuntimeException("noooooooooooooooooooo");
+        }
+    }
+
+
+    @GetMapping("/createdirectory")
+    public  Object createdirectory(){
+
+        try {
+              File file = new File("E:\\fresh files\\new\\sabari");
+              if(!file.exists()){
+                   if(file.mkdirs()){
+                        return "Successfully directory created";
+                   }
+                   else {
+                          return "cannot be able to create directory";
+                   }
+              }
+              return "Directory Already exist";
+
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Exception thrown using try and catch method");
+        }
+
+    }
+
+    private final String baseFolderStr = "Files/upload";
+    private final Path rootLocation = Paths.get(baseFolderStr);
+
+    @GetMapping("/getpath")
+    public  Object getpath(@RequestParam MultipartFile file123){
+
+        try {
+            byte[] array = file123.getBytes();
+            File file1 = new File(String.valueOf(rootLocation));
+            if(!file1.exists()){
+                if(file1.mkdirs()){
+                    System.out.println("Successfully directory created");
+                }
+                else {
+                    System.out.println("cannot be able to create directory");
+                }
+            }
+            else {
+                System.out.println("Directory Already exist");
+            }
+            File file = new File(rootLocation+"//"+file123.getOriginalFilename());
+            if(file.createNewFile()){
+                System.out.println("File created");
+                FileOutputStream fileOutputStream = new FileOutputStream(file.getPath());
+                System.out.println("Destiny path is " + file.getPath());
+                fileOutputStream.write(array);
+                fileOutputStream.close();
+            }else {
+                System.out.println("File cannot be able to create");
+            }
+
+           String fileName = file123.getOriginalFilename();
+           return fileName;
+
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Exception thrown using try and catch method");
+        }
+
+    }
+
+
+
 
 }
